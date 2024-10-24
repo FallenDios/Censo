cb3955b7f63bff9b8f6307fca5a30281d5af618e
FallenDios <172858105+FallenDios@users.noreply.github.com>
2024-10-24 00:24:00 -0300

    eje18.php

--- eje18.php
+++ eje18.php
@@ -1,5 +1,4 @@
 <?php
-
 /*Alg que se utiliza en un censo para saber viviendas con menores, total de     
 habitantes y la cantidad maxima de menores*/
 
@@ -10,7 +9,6 @@ FLOAT $promedioHabitantes
 
 STRING $jefeConMasMenores, $jefeDeHogar */
 
-
 $encuestaFormuladas=0;
 $viviendasConMenores=0;
 $totalHabitantes=0;
@@ -20,7 +18,10 @@ $jefeConMasMenores="";
 echo"Ingrese la cantidad de encuesta a realizar\n";
 $totalEscuesta=trim(fgets(STDIN));
 
-for ($i=1; $i<=$totalEscuesta; $i++){
+if($totalEscuesta <0){
+    echo"No se ingresaron encuestas";
+}else{
+    for ($i=1; $i<=$totalEscuesta; $i++){
     echo"Ingrese el nombre del jefe del hogar\n";
     $jefeDeHogar=trim(fgets(STDIN));
     echo"Ingrese la cantidad de habitantes de la vivienda\n";
@@ -35,13 +36,11 @@ for ($i=1; $i<=$totalEscuesta; $i++){
     if($cantidadMenores > $maxMenores){
         $viviendasConMenores= $viviendasConMenores+1;
     }
-
     if($cantidadMenores > $maxMenores){
         $maxMenores= $cantidadMenores;
         $jefeConMasMenores= $jefeDeHogar;
     }
 }
-
 if($totalEscuesta>0){
     $promedioHabitantes= $totalHabitantes/$totalEscuesta;
 }else{
@@ -52,6 +51,9 @@ echo"La cantidad de encuestas formuladas es: $encuestaFormuladas\n";
 echo"La cantidad de viviendas con menores es: $viviendasConMenores\n";
 echo"El promedio de habitantes por encuesta es: $promedioHabitantes\n";
 echo"El jefe con mas menores es: $jefeConMasMenores\n";
+}
+    
+
 
 
 ?>
